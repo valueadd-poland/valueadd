8e02c2058e6b1717d0c391e224c1c9b4fbd1cc7b
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-10-16 16:09:00 +0200

    fix(validation-messages): validationMessagesService is duplicated
    Revert providedIn 'root' for ValidationMessagesService.
    Remove providing ValidationMessagesService in module.
    
    resolve #NV-24

--- libs/validation-messages/src/lib/validation-messages.module.ts
+++ libs/validation-messages/src/lib/validation-messages.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValidationMessagesComponent } from './components/validation-messages/validation-messages.component';
-import { ValidationMessagesService } from './services/validation-messages.service';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ValidationMessagesComponent],
-  exports: [ValidationMessagesComponent],
-  providers: [ValidationMessagesService]
+  exports: [ValidationMessagesComponent]
 })
 export class ValidationMessagesModule {}
