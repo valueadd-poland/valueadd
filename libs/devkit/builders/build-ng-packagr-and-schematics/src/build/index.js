58dd0460b62986d0b477a6fb3b2240ccab1ae224
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2019-11-22 13:17:54 +0100

    working

--- libs/devkit/builders/build-ng-packagr-and-schematics/src/build/index.js
+++ libs/devkit/builders/build-ng-packagr-and-schematics/src/build/index.js
@@ -1,40 +1,23 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-var tslib_1 = require('tslib');
 var architect_1 = require('@angular-devkit/architect');
-var path_1 = require('path');
 var rxjs_1 = require('rxjs');
 var operators_1 = require('rxjs/operators');
 // @ts-ignore
 var schema_1 = require('./schema');
 exports.NgPackagrBuilderOptions = schema_1.Schema;
-function initialize(options, root) {
-  return tslib_1.__awaiter(this, void 0, void 0, function() {
-    var packager;
-    return tslib_1.__generator(this, function(_a) {
-      switch (_a.label) {
-        case 0:
-          return [
-            4 /*yield*/,
-            Promise.resolve().then(function() {
-              return require('ng-packagr');
-            })
-          ];
-        case 1:
-          packager = _a.sent().ngPackagr();
-          packager.forProject(path_1.resolve(root, options.project));
-          if (options.tsConfig) {
-            packager.withTsConfig(path_1.resolve(root, options.tsConfig));
-          }
-          return [2 /*return*/, packager];
-      }
-    });
-  });
-}
+var ng_packagr_build_1 = require('./ng-packagr-build');
+var schematics_build_1 = require('./schematics.build');
 function execute(options, context) {
-  return rxjs_1.from(initialize(options, context.workspaceRoot)).pipe(
+  return rxjs_1.from(ng_packagr_build_1.initialize(options, context.workspaceRoot)).pipe(
     operators_1.switchMap(function(packager) {
       return options.watch ? packager.watch() : packager.build();
+    }),
+    operators_1.switchMap(function() {
+      return schematics_build_1.initialize(options, context.workspaceRoot);
+    }),
+    operators_1.switchMap(function(builder) {
+      return builder.build();
     }),
     operators_1.mapTo({ success: true })
   );
--- libs/devkit/builders/build-ng-packagr-and-schematics/src/build/index.ts
+++ libs/devkit/builders/build-ng-packagr-and-schematics/src/build/index.ts
@@ -1,31 +1,19 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
-import { resolve } from 'path';
-import { Observable, from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { mapTo, switchMap } from 'rxjs/operators';
 // @ts-ignore
 import { Schema as NgPackagrBuilderOptions } from './schema';
-
-async function initialize(
-  options: NgPackagrBuilderOptions,
-  root: string
-): Promise<import('ng-packagr').NgPackagr> {
-  const packager = (await import('ng-packagr')).ngPackagr();
-
-  packager.forProject(resolve(root, options.project));
-
-  if (options.tsConfig) {
-    packager.withTsConfig(resolve(root, options.tsConfig));
-  }
-
-  return packager;
-}
+import { initialize as initializeNgPackagrBuilder } from './ng-packagr-build';
+import { initialize as initializeSchematicsBuilder } from './schematics.build';
 
 export function execute(
   options: NgPackagrBuilderOptions,
   context: BuilderContext
 ): Observable<BuilderOutput> {
-  return from(initialize(options, context.workspaceRoot)).pipe(
+  return from(initializeNgPackagrBuilder(options, context.workspaceRoot)).pipe(
     switchMap(packager => (options.watch ? packager.watch() : packager.build())),
+    switchMap(() => initializeSchematicsBuilder(options, context.workspaceRoot)),
+    switchMap(builder => builder.build()),
     mapTo({ success: true })
   );
 }
